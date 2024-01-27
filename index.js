b4a1736092de89b9dfcd59843f5fc787e28a8a21
unknown <alex.naziru.dev@gmail.com>
2024-01-27 16:59:27 +0200

    finished app

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+// This is built in js function
+const {hash} = window.location
+const message =  atob(hash.replace("#", ""))
+if (message) {
+    document.querySelector("#message-form").classList.add("hide")
+    document.querySelector("#message-show").classList.remove("hide")
+    document.querySelector("h1").innerHTML = message
+}
+
 // This prevents the default html to send an empty form
 document.querySelector("form").addEventListener("submit", ev => {
     ev.preventDefault()
+
+    // Toggling visibility
+    document.querySelector("#message-form").classList.add("hide")
+    document.querySelector("#link-form").classList.remove("hide")
 
     const input = document.querySelector("#msg-input")
     // Now we get the user input and convert it to base64 characters
